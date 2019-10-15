461f6f1b4bdb47b59a3749fb523e3737732ce5a7
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-15 20:45:00 +0530

    All tests passed

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'dart:io';
 // Challenge 1
 // Create a function that returns the total number of steps it takes to transform each element to the maximal element in the list.
 // Each step consists of incrementing a digit by one.
@@ -7,7 +9,19 @@
 // incrementToTop([4, 3, 4]) ➞ 1
 // incrementToTop([3, 3, 3]) ➞ 0
 // incrementToTop([3, 10, 3]) ➞ 14
-
+int incrementToTop(List <int> list)
+{
+  list.sort();
+  int operations=0;
+  int max=list[list.length-1];
+  for(int i=0;i<list.length;i++) {
+    while (list[i] < max) {
+      list[i] += 1;
+      operations++;
+    }
+  }
+  return operations;
+}
 // Challenge 2
 // In BlackJack, cards are counted with -1, 0, 1 values:
 //
@@ -23,5 +37,29 @@
 
 // Challenge 3
 // Write a test case each for previous two functions you wrote
+int count(List list)
+{
+  int c=0;
+  for(int i=0;i<list.length;i++){
+    if(list[i]==2 || list[i]==3 || list[i]== 4 || list[i]==5 || list[i]==6)
+      {
+        c+=1;
+      }
+    else if(list[i]==7 || list[i]==8 || list[i]== 9)
+    {
 
-main() {}
+    }
+    else if(list[i]==10 || list[i]=='J' || list[i]== 'Q' || list[i]=='K' || list[i]== 'A')
+      {
+        c-=1;
+      }
+  }
+  return c;
+}
+
+main() {
+  List<int> list1=[3,4,5];
+  List list2=[5, 9, 10, 3, "J", "A", 4, 8, 5];
+  print(incrementToTop(list1));
+  print(count(list2));
+}
--- bin/test.dart
+++ bin/test.dart
@@ -1,3 +1,20 @@
+import 'dart:math';
+import 'main.dart';
+
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  List<int> list1=[3,4,5];
+  List list2=[5, 9, 10, 3, "J", "A", 4, 8, 5];
+  test('Increment to Top test', (){
+    expect(incrementToTop([3,4,5]),3);
+    expect(incrementToTop([4,3,4]),1);
+    expect(incrementToTop([3,3,3]),0);
+    expect(incrementToTop([3,10,3]),14);
+  });
+  test('counttest', (){
+    expect(count([5, 9, 10, 3, "J", "A", 4, 8, 5]),1);
+    expect(count(["A", "A", "K", "Q", "Q", "J"]),-6);
+    expect(count(["A", 5, 5, 2, 6, 2, 3, 8, 9, 7]),5);
+  });
+}
